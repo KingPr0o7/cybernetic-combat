7f51b819798866a973bae8a3c2b7442723841536
Nathan Parker <natecoleparker@gmail.com>
2024-12-15 19:51:50 -0500

    🎨 Multiplayer Colors!
    - Users can now change their skin color to any color

--- scenes/scripts/character_selector.gd
+++ scenes/scripts/character_selector.gd
@@ -1,11 +1,42 @@
 extends Control
 
+@onready var player_dummy = $host_dummy
+@onready var enemy_dummy = $enemy_dummy
+
 @onready var host_username = $host_username_area/host_username
 @onready var enemy_username = $enemy_username_area/enemy_username
+
+@onready var amethystine = $center_container/first_color_set/first_color_pair/amethystine
+@onready var plumshade = $center_container/first_color_set/first_color_pair/plumshade
+@onready var cerulia = $center_container/first_color_set/second_color_pair/cerulia
+@onready var deepwave = $center_container/first_color_set/second_color_pair/deepwave
+@onready var scarflare = $center_container/first_color_set/third_color_pair/scarflare
+@onready var crimsonite = $center_container/first_color_set/third_color_pair/crimsonite
+
+@onready var greenvive = $center_container/second_color_set/first_color_pair/greenvive
+@onready var terrain = $center_container/second_color_set/first_color_pair/terrain
+@onready var sienna = $center_container/second_color_set/second_color_pair/sienna
+@onready var bronzearth = $center_container/second_color_set/second_color_pair/bronzearth
+@onready var graphite = $center_container/second_color_set/third_color_pair/graphite
+@onready var midnight = $center_container/second_color_set/third_color_pair/midnight
 
 func _ready() -> void:
 	host_username.editable = false
 	enemy_username.editable = false
+
+	amethystine.pressed.connect(_on_color_call.bind(amethystine, "amethystine"))
+	plumshade.pressed.connect(_on_color_call.bind(plumshade, "plumshade"))
+	cerulia.pressed.connect(_on_color_call.bind(cerulia, "cerulia"))
+	deepwave.pressed.connect(_on_color_call.bind(deepwave, "deepwave"))
+	scarflare.pressed.connect(_on_color_call.bind(scarflare, "scarflare"))
+	crimsonite.pressed.connect(_on_color_call.bind(crimsonite, "crimsonite"))
+
+	greenvive.pressed.connect(_on_color_call.bind(greenvive, "greenvive"))
+	terrain.pressed.connect(_on_color_call.bind(terrain, "terrain"))
+	sienna.pressed.connect(_on_color_call.bind(sienna, "sienna"))
+	bronzearth.pressed.connect(_on_color_call.bind(bronzearth, "bronzearth"))
+	graphite.pressed.connect(_on_color_call.bind(graphite, "graphite"))
+	midnight.pressed.connect(_on_color_call.bind(midnight, "midnight"))
 
 func input_controller(username: LineEdit, cursor: Node2D, cursor_id: int, mode: String) -> void:
 	username.grab_focus()
@@ -28,7 +59,6 @@ func input_controller(username: LineEdit, cursor: Node2D, cursor_id: int, mode: 
 				rpc("set_host_username", username.text)
 			if username.name == "enemy_username" and cursor_id != 1 and username.text != "":
 				username.editable = false
-				print(enemy_username.text)
 				rpc_id(1, "set_enemy_username", username.text)
 
 @rpc("any_peer")
@@ -38,6 +68,14 @@ func set_host_username(new_username: String) -> void:
 @rpc("any_peer")
 func set_enemy_username(new_username: String) -> void:
 	enemy_username.text = new_username
+
+@rpc("any_peer")
+func set_host_color(new_color: String) -> void:
+	player_dummy.set_skin(new_color)
+
+@rpc("any_peer")
+func set_enemy_color(new_color: String) -> void:
+	enemy_dummy.set_skin(new_color)
 
 func _on_host_username_area_area_entered(area: Area2D) -> void:
 	input_controller(host_username, area, area.get_parent().cursor_index, "enter")
@@ -50,3 +88,15 @@ func _on_enemy_username_area_area_entered(area: Area2D) -> void:
 
 func _on_enemy_username_area_area_exited(area: Area2D) -> void:
 	input_controller(enemy_username, area, area.get_parent().cursor_index, "exit")
+
+func _on_color_call(button: TextureButton, color: String) -> void:
+	var area2d = button.get_node("%s_area" % color)
+	var overlapping_areas = area2d.get_overlapping_areas()
+	for area in overlapping_areas:
+		var cursor_id = area.get_parent().cursor_index
+		if cursor_id == 1:
+			player_dummy.set_skin(color)
+			rpc("set_host_color", color)
+		else:
+			enemy_dummy.set_skin(color)
+			rpc_id(1, "set_enemy_color", color)
--- scenes/scripts/dummy.gd
+++ scenes/scripts/dummy.gd
@@ -2,7 +2,15 @@
 extends SpineSprite
 
 @export var current_animation = "" # Send to Synchronizer 
+
+@export_enum("Block", "Charged Kick", "Die Behind", "Die Front", "Fall", "Fight Stance", "Hurt", "Idle", "Jab Double", "Jab Single", "Jab Thousands", "Jump", "Kick", "Land", "Launch", "Run", "Run Stop", "Walk") var load_animation
 @export var direction = 1
+@export_enum("Amethystine", "Plumshade", "Cerulia", "Deepwave", "Scarflare", "Crimsonite", "Greenvive", "Terrain", "Sienna", "Bronzearth", "Graphite", "Midnight") var color
+
+const COLORS = ["Amethystine", "Plumshade", "Cerulia", "Deepwave", "Scarflare", "Crimsonite", "Greenvive", "Terrain", "Sienna", "Bronzearth", "Graphite", "Midnight"]
+const ANIMATIONS = ["Block", "Charged Kick", "Die Behind", "Die Front", "Fall", "Fight Stance", "Hurt", "Idle", "Jab Double", "Jab Single", "Jab Thousands", "Jump", "Kick", "Land", "Launch", "Run", "Run Stop", "Walk"]
+
+var is_initialized = false
 
 func play_animation(animation_name: String, loop: bool, track: int, reverse: bool = false, additive: bool = false, delay: float = 0.00):
 	"""
@@ -13,7 +21,7 @@ func play_animation(animation_name: String, loop: bool, track: int, reverse: boo
 	"""
 	
 	var track_entry
-
+	
 	# Only play the animation if it's not already playing
 	if current_animation != animation_name:
 		current_animation = animation_name
@@ -33,16 +41,26 @@ func play_animation(animation_name: String, loop: bool, track: int, reverse: boo
 				track_entry = self.get_animation_state().add_animation(animation_name, delay, loop, track)
 			
 			track_entry.set_reverse(true)
+		
+		if loop == true and self.get_animation_state().get_num_tracks() == 0:
+			if additive == false:
+				track_entry = self.get_animation_state().set_animation(animation_name, loop, track)
+			else:
+				track_entry = self.get_animation_state().add_animation(animation_name, delay, loop, track)		
 
-func set_skin(color: String):
+func set_skin(new_color: String):
 	"""
 	Set's the stickman's skin color for the player. There is a set of 12
 	available colors to choose from, defined at the const of COLORS.
 	"""
 
-	var skin = self.get_skeleton().get_data().find_skin(color)
+	var skin = self.get_skeleton().get_data().find_skin(new_color)
 	self.get_skeleton().set_skin(skin)
 
 func _process(_delta):
-	if direction == -1:
+	if is_initialized == false:
 		self.get_skeleton().set_scale_x(direction)
+		self.set_skin(COLORS[color].to_lower())
+		is_initialized = true
+	
+	self.play_animation(ANIMATIONS[load_animation].to_lower().replace(" ", "_"), true, 0, false)
